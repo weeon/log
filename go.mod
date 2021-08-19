a7e7332a3d69c564e5dd6dc0874dd025b8d6f9ae
orvice <orvice@gmail.com>
2021-08-19 12:13:43 +0800

    add nats writer

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.12
 
 require (
 	github.com/go-redis/redis/v8 v8.5.0
+	github.com/nats-io/nats-server/v2 v2.3.4 // indirect
+	github.com/nats-io/nats.go v1.11.1-0.20210623165838-4b75fc59ae30
 	github.com/weeon/contract v0.0.0-20190520152601-a4ee53bdb563
 	go.uber.org/zap v1.15.0
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
--- writer.go
+++ writer.go
@@ -3,6 +3,7 @@ package log
 import (
 	"context"
 	"github.com/go-redis/redis/v8"
+	"github.com/nats-io/nats.go"
 	"go.uber.org/zap/zapcore"
 )
 
@@ -27,5 +28,28 @@ func (w *RedisWriter) Write(p []byte) (int, error) {
 }
 
 func (w *RedisWriter) Sync() error {
+	return nil
+}
+
+// nats
+type NatsWriter struct {
+	nc      *nats.Conn
+	subject string
+}
+
+func NewNatsWriter(natsURL string, subject string) *NatsWriter {
+	nc, _ := nats.Connect(natsURL)
+	return &NatsWriter{
+		nc:      nc,
+		subject: subject,
+	}
+}
+
+func (n *NatsWriter) Write(p []byte) (int, error) {
+	err := n.nc.Publish(n.subject, p)
+	return len(p), err
+}
+
+func (n *NatsWriter) Sync() error {
 	return nil
 }
